166b17cfbb0626fb2aa921e7f756204f9957c077
EnesT51 <99040669@mydavinci.nl>
2023-02-17 11:33:43 +0100

    fibonacci reeks opdr afgerond

--- fibonaci.js
+++ fibonaci.js
@@ -6,10 +6,8 @@ function fibonaci(num)
         if(i < 2){list.push(i); }
         else
         {
-          var x = list[i -1] + list[i -2];
-          list.push(x);
+          list.push(list[i -1] + list[i -2]);
         }
       }
       return list;
     }
-console.log(fibonaci(20));
